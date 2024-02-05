936d244eabc00f4fb77030c4c45679db266d20f9
okkmnone <156905063+okkmnone@users.noreply.github.com>
2024-02-05 21:36:33 +0800

    Script fixes due to changes in directory structure

--- build/web_run.sh
+++ build/web_run.sh
@@ -1,5 +1,9 @@
 # rustup target install wasm32-unknown-unknown
 # cargo install wasm-server-runner
 
-export WASM_SERVER_RUNNER_ADDRESS=127.0.0.1
+trap "popd; exit 1" SIGINT
+
+pushd ../
+export WASM_SERVER_RUNNER_ADDRESS=0.0.0.0
 cargo run --release --target wasm32-unknown-unknown --no-default-features --features "experimental_channel" --bin ethertia
+popd
